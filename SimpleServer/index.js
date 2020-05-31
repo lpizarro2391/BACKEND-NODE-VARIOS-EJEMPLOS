458acea2e5b151ff48ac194f1dd21d4218e5ba7d
lpizarro <lpizarro2391@github.com>
2020-05-30 21:58:14 -0600

    crear el servidor con la funcion create server del modulo http, y lanzaremos el servidor con la funcion listen, y le pasamos 2 parametros el puerto por donde esat disponible el servidory el otro que es un callback que se levante el servidor

--- SimpleServer/index.js
+++ SimpleServer/index.js
@@ -5,3 +5,8 @@ var PORT = 8880
 function handleRequest(req,res){
     res.end('Funcionando correctamente, Recurso pedido:'+req.url)
 }
+
+var Server = http.createServer(handleRequest)
+server.listen(PORT, function () {
+    console.log('Server is listening on port:'+PORT)
+})
